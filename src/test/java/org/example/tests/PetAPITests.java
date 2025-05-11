da712255f7097fa01cd9ce711c8e73d2c8aa51c3
Alex <sasham2@yandex.ru>
2025-05-11 17:50:47 -0400

    add first parametrized test

---
+++ src/test/java/org/example/tests/PetAPITests.java
@@ -0,0 +1,50 @@
+package org.example.tests;
+
+
+import com.example.petstore.model.Pet;
+import org.assertj.core.api.SoftAssertions;
+import org.example.RestClient;
+import org.example.extensions.RestClientResolver;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+@ExtendWith({
+        RestClientResolver.class,
+})
+public class PetAPITests {
+    @ParameterizedTest
+    @MethodSource("getPetIds")
+    void testGetPetById(int petId, RestClient client) {
+        Pet pet = client.get("/pet/", petId, 200).as(Pet.class);
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(pet.getId())
+                .as("Check pet ID")
+                .isEqualTo(petId);
+        softly.assertThat(pet.getStatus().getValue())
+                .as("Check pet status")
+                .isIn("available", "pending", "sold");
+        softly.assertThat(pet.getCategory())
+                .as("Category should not be null")
+                .isNotNull();
+        softly.assertThat(pet.getPhotoUrls())
+                .as("Photo URLs")
+                .isNotEmpty();
+        softly.assertThat(pet.getTags())
+                .as("Tags list")
+                .isNotEmpty();
+        softly.assertAll();
+    }
+
+    static Stream<Arguments> getPetIds() {
+        return Stream.of(
+                Arguments.of(111),
+                Arguments.of(12345),
+                Arguments.of(3)
+        );
+    }
+}
+
